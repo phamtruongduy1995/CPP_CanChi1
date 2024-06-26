13c9d0375d11b8f7c53d49b15fd12bcd34377b88
SPC <SPC@23-LAB402>
2024-06-26 19:16:40 +0700

    Add project files.

---
+++ CPP_CanChi1/CPP_CanChi1.cpp
@@ -0,0 +1,23 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string can[10] = { "Canh" ,"Tan", "Nham", "Quy", "Giap" ,"at", "Binh", "?inh", "Mau", "ky" };
+    string chi[12] = { "Than" ,"Dau", "Tuat", "Hoi", "Ty" ,"Suu", "Dan", "Mao", "Thin", "Ti", "Ngo", "Mui" };
+
+
+    int nam;
+    cout << " Nhap nam: ";
+    cin >> nam;
+
+    string cn = can[nam % 10];
+    string ci = chi[nam % 12];
+
+    cout << cn << " " << ci;
+
+    return 0;
+
+}
